aaf72754c46a669426b575d869589793e225acb9
Jasper Wiegratz <jwhy@jwhy.de>
2013-10-17 10:20:32 +0200

    Implement number generator (~CNS.java) and filter (UniqueNumber.java)
    Signed-off-by: JWhy <jwhy@jwhy.de>

---
+++ src/de/jwiegratz/unique_numbers/CustomNumberSystem.java
@@ -0,0 +1,64 @@
+package de.jwiegratz.unique_numbers;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class CustomNumberSystem {
+
+	private ArrayList<String> numbers;
+	private int base;
+
+	Timer timer = new Timer();
+
+	public CustomNumberSystem(int base) {
+		timer.scheduleAtFixedRate(new MemoryInfo(), 0, 1000);
+		System.out.println("Max mem: " + Runtime.getRuntime().maxMemory()/1000 + "K");
+		
+		this.numbers = new ArrayList<String>();
+		this.base = base;
+	}
+
+	public static long faculty(int n) {
+		long fac = 1;
+		for (int i = 1; i <= n; i++) {
+			fac = fac * i;
+		}
+		return fac;
+	}
+
+	public String[] getAllPossibilities() {
+		//Leere Zahlen-Array
+		this.numbers.clear();
+		
+		//Starte rekursive Zahlensystem-Funktion
+		this.base_count(base, 0, new String());
+		
+		//Gebe ArrayList als Array zurück
+		String[] result = this.numbers.toArray(new String[this.numbers.size()]);
+		return result;
+	}
+
+	private void base_count(int base, int depth, String progress) {
+		
+		//Speichere fertige Zahl, wenn in der untersten Baum-Ebene
+		if (depth == base) {
+			this.numbers.add(progress);
+		} else if (depth <= base) {
+			for (int i = 1; i <= base; i++) {
+				//Rekursiver Aufruf, Anzahl so groß wie die Basis (Basis 3 => 3 rek. Aufrufe)
+				base_count(base, depth + 1, progress.concat(new Integer(i).toString() + " "));
+			}
+		}
+	}
+	
+	class MemoryInfo extends TimerTask {
+		Runtime rt = Runtime.getRuntime();
+	    public void run() {
+	    	if(numbers != null){
+	    		System.out.println("Used: " + ((rt.maxMemory() - rt.freeMemory())/ 1000)
+	    				+ "K  Free: " + (rt.freeMemory() / 1000) + "K" + "  " + numbers.size() + " Results");
+	    	}
+	    }
+	 }
+
+}
---
+++ src/de/jwiegratz/unique_numbers/UniqueNumbers.java
@@ -0,0 +1,65 @@
+package de.jwiegratz.unique_numbers;
+import java.util.HashSet;
+
+
+public class UniqueNumbers {
+	
+	public String[] getUniqueNumbers(int base){
+		//Ermittle alle möglichen Nummern des Zahlensystems
+		CustomNumberSystem cns = new CustomNumberSystem(base);
+		String[] numbers = cns.getAllPossibilities();
+		
+		//Lege Array für einmalige Nummern an.
+		String[] unumbers = new String[(int) CustomNumberSystem.faculty(base)];
+		int pos = 0;
+		
+		for(String number : numbers){
+			if(this.checkUnique(number)){
+				//Wenn aktuelle Zahl "querziffer-rein", speichere in Array
+				unumbers[pos] = number;
+				pos++;
+			}
+		}
+		return unumbers;
+	}
+	
+	private boolean checkUnique(String number) {
+		//HashSet erzeugen. Funktioniert wie ein dynamisches Array
+		//Werte dürfen nur einmalig vorkommen
+		HashSet<String> unicache = new HashSet<String>();
+		
+		//Zerlege Zahl in "Ziffern"
+		String[] tnt = number.split(" ");
+		
+		for(String t : tnt){
+			//Prüfe, ob Ziffer schon vorgekommen ist
+			if(unicache.contains(t)){
+				//Ziffern haben sich wiederholt, Ergebnis negativ
+				return false;
+			}
+			//Füge Ziffer hinzu, wenn diese noch nicht im Set ist
+			unicache.add(t);
+		}
+		
+		//Ziffern haben sich nicht wiederholt, Ergebnis positiv
+		return true;
+	}
+
+	public static void main(String[] args){
+		int base = 4;
+		
+		if (args.length > 0) {
+		    try {
+		        base = Integer.parseInt(args[0]);
+		    } catch (NumberFormatException e) {
+		        System.err.println("Argument" + " must be an integer");
+		        System.exit(1);
+		    }
+		}
+		
+		UniqueNumbers un = new UniqueNumbers();
+		for(String num : un.getUniqueNumbers(base)){
+			System.out.println(num);
+		}
+	}
+}
