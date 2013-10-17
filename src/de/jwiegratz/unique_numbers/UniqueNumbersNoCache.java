3113b5a5e2f63622b58dfc32980ea48005a15a1d
Jasper Wiegratz <jwhy@jwhy.de>
2013-10-17 10:21:14 +0200

    Implement low-memory optimized class that filters BEFORE saving
    Signed-off-by: JWhy <jwhy@jwhy.de>

---
+++ src/de/jwiegratz/unique_numbers/UniqueNumbersNoCache.java
@@ -0,0 +1,78 @@
+package de.jwiegratz.unique_numbers;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class UniqueNumbersNoCache {
+
+	private int base;
+	private ArrayList<String> numbers;
+	
+	public UniqueNumbersNoCache(int base){
+		this.base = base;
+		this.numbers = new ArrayList<String>();
+	}
+
+	public String[] calculate() {
+		this.numbers.clear();
+		this.base_count(base, 0, new String());
+		
+		String[] ret = new String[this.numbers.size()];
+		ret = this.numbers.toArray(ret);
+		
+		return ret;
+	}
+
+	private void base_count(int base, int depth, String progress) {
+		
+		//Speichere fertige Zahl, wenn in der untersten Baum-Ebene
+		if (depth == base) {
+			if(this.filterCombo(progress) == true){
+				this.numbers.add(progress);
+			}
+		} else if (depth <= base) {
+			for (int i = 1; i <= base; i++) {
+				//Rekursiver Aufruf, Anzahl so groß wie die Basis (Basis 3 => 3 rek. Aufrufe)
+				base_count(base, depth + 1, progress.concat(new Integer(i).toString() + " "));
+			}
+		}
+	}
+	
+	private boolean filterCombo(String progress) {
+
+		HashSet<String> unicache = new HashSet<String>();
+		
+		//Zerlege Zahl in "Ziffern"
+		String[] tnt = progress.split(" ");
+		
+		for(String t : tnt){
+			//Prüfe, ob Ziffer schon vorgekommen ist
+			if(unicache.contains(t)){
+				//Ziffern haben sich wiederholt, Ergebnis negativ
+				return false;
+			}
+			//Füge Ziffer hinzu, wenn diese noch nicht im Set ist
+			unicache.add(t);
+		}
+		
+		return true;
+	}
+
+	public static void main(String[] args){
+
+		int base = 4;
+		if (args.length > 0) {
+		    try {
+		        base = Integer.parseInt(args[0]);
+		    } catch (NumberFormatException e) {
+		        System.err.println("Argument" + " must be an integer");
+		        System.exit(1);
+		    }
+		}
+
+		UniqueNumbersNoCache unnc = new UniqueNumbersNoCache(base);
+		for(String num : unnc.calculate()){
+			System.out.println(num);
+		}
+	}
+	
+}
